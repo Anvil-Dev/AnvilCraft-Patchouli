1f9ff586b791ad3e71b17fd51506afb36dc60d82
QiuShui1012 <150409561+qiushui1012@users.noreply.github.com>
2025-06-22 20:50:17 +0800

    修复伤害值、修复电荷产生位置、完成中文手册内容

---
+++ src/main/java/dev/dubhe/anvilcraft/util/AabbUtil.java
@@ -0,0 +1,32 @@
+package dev.dubhe.anvilcraft.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.SectionPos;
+import net.minecraft.world.phys.AABB;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AabbUtil {
+    /**
+     * 从方块坐标创建
+     */
+    public static AABB create(BlockPos a, BlockPos b) {
+        return new AABB(a.getX(), a.getY(), a.getZ(), b.getX() + 1, b.getY() + 1, b.getZ() + 1);
+    }
+
+    public static AABB centerSectionTo3x3x3(BlockPos pos) {
+        return centerSectionTo3x3x3(SectionPos.of(pos));
+    }
+
+    public static AABB centerSectionTo3x3x3(SectionPos center) {
+        return new AABB(
+            center.minBlockX() - 16,
+            center.minBlockY() - 16,
+            center.minBlockZ() - 16,
+            center.maxBlockX() + 16 + 1,
+            center.maxBlockY() + 16 + 1,
+            center.maxBlockZ() + 16 + 1
+        );
+    }
+}
