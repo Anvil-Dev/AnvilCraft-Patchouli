6ef85085f0bc01d46dc52732cfb31b060ef0605d
TBPig <147127248+TBPig@users.noreply.github.com>
2025-10-22 12:02:10 +0800

    手册修复水泥配方结果显示错误

--- src/main/java/dev/dubhe/anvilcraft/integration/patchouli/page/anvilitem/PageBulging.java
+++ src/main/java/dev/dubhe/anvilcraft/integration/patchouli/page/anvilitem/PageBulging.java
@@ -1,7 +1,6 @@
 package dev.dubhe.anvilcraft.integration.patchouli.page.anvilitem;
 
 import dev.dubhe.anvilcraft.init.reicpe.ModRecipeTypes;
-import dev.dubhe.anvilcraft.recipe.anvil.predicate.block.HasCauldron;
 import dev.dubhe.anvilcraft.recipe.anvil.wrap.BulgingRecipe;
 import dev.dubhe.anvilcraft.util.CauldronUtil;
 import dev.dubhe.anvilcraft.util.RenderHelper;
@@ -35,7 +34,7 @@ public class PageBulging extends PageAnvilItemProcess<BulgingRecipe> {
         if (recipe.isProduceFluid()) {
             return Blocks.CAULDRON.defaultBlockState();
         } else {
-            return CauldronUtil.fullState(HasCauldron.getDefaultCauldron(recipe.getHasCauldron().fluid()));
+            return recipe.getHasCauldron().getTransformCauldron().defaultBlockState();
         }
     }
 }
